1cabffbc812040cf174c2aabc79833d5acad91ee
nerotix <vierkantje80@gmail.com>
2016-07-02 12:32:21 +0200

    updated with values from VT and comments added

--- yapfsvt.py
+++ yapfsvt.py
@@ -83,7 +83,14 @@ def int2ip(int_ip):
 
 
 def main():
-
+    """
+    This function will first call for the function to check the integrity of the db and hashes.
+    Next it will assign the values that are found in the config file to the variable eth. Then
+    it will start listening to UDP port 53 to catch all the traffic that arrives there. Next,
+    it will filter out all the A records that are in the dns response and get the dns names
+    that are connected to that A records. Lastly it will get the current system time and
+    call for the function writes the data to redis.
+    """
     checkIntegrity()
     # grabs the eth port to bind and the list of ignored domains from the config using the config.py module.
     eth = config.getSetting('setup', 'eth')
@@ -113,8 +120,7 @@ def main():
                                 srcip = int2ip(struct.unpack('>I', answer.rdata)[0])
                             else:
                                 continue
-                except Exception as e:
-                    #print e
+                except Exception:
                     continue
                 else:
                     # dns
@@ -123,6 +129,12 @@ def main():
                 ltime = now.strftime("%Y-%m-%dT%H:%M:%S") + ".%03d" % (now.microsecond / 1000) + "Z"
                 toRedis(dstip, srcip, dnsname, ltime)
 
+"""
+This checks what the value is from the last stored records in MongoDB
+if the dictionary contains more then 0 records, the teller will be the number
+of the value stored at the key "_id". If the length of the dictionary is 0,
+teller will be assigned the value of 0.
+"""
 db = mongoClient.yapdns
 idList = list(db.dnsinfo.find({}, "_id"))
 if idList.__len__() > 0:
@@ -132,6 +144,21 @@ else:
 
 
 def toRedis(dstip, srcip, dnsname, timestamp):
+    """
+    This function check whether the dns name is in the list ignoreDom,
+    which is located in the config file. If the dnsname is in that list,
+    the package is ignored. If it's not in that list, the package is handled.
+    The teller will be upped by 1 every time the package is handled and
+    it will created 3 threads, vtThread will start the VTHandler function, the
+    fsThread will start the FSHandler function and both will then write it's data
+    to the Redis server. The  awThread will gives the value's of the other 2
+    threads and the teller to a new thread that watches whether the running threads
+    are done.
+    :param dstip: destination to where the dns package is going to
+    :param srcip: IP-address from where the dns package originated
+    :param name: dns name from the dns package response
+    :param timestamp: current system time
+    """
     global teller
 
     ignoreDom = config.getSetting('setup', 'ignore')
@@ -183,17 +210,30 @@ def checkIntegrity():
                 print id + " does not check out"
 
 
-
-
-
 def apiWatcher(vtThread, fsThread, id):
+    """
+    This function will wait until both the threads are finished, once
+    the threads are finished, it will start the function toMongo with
+    the value from id.
+    :param vtThread: thread that handles the responses from VirusTotal
+    :param fsThread: thread that handles the responses from Farsight
+    :param id: value of teller from the function toRedis
+    """
     vtThread.join()
     fsThread.join()
     toMongo(id)
-    #print "ik heb iets naar mongo geschreven"
 
 
 def FSHandler(srcip):
+    """
+    This function creates an object from the farsight script with the values
+    that are found in the config file. After the object is created, the object
+    does a request with the param to the farsight database. Next, it checks how
+    many company's are attached to 1 IP-address, if that is more than 5, it will
+    send the value "FastFlux" to the Redis database, else it will send the value
+    "Clean" to the database.
+    :param srcip: IP-address from where the dns package came from
+    """
     global ipTeller
 
     request = fsReq.DnsdbClient(
@@ -223,7 +263,14 @@ def FSHandler(srcip):
 
 
 def VTHandler(name):
-
+    """
+    This function create an object of the VirusTotal script with the values
+    that are found in the config file. After the object is created, the object
+    does a request with the param. Lastly it checks what the value is of the
+    positives hits in the answer.
+    :param name: name from dns package
+    :return: value based on the number of hits that VirusTotal gives
+    """
     request = vtReq.VirusTotalQuery(
         endpoint=config.getSetting("virustotal", "endpoint"),
         apikey=config.getSetting("virustotal", "key"),
@@ -238,13 +285,19 @@ def VTHandler(name):
         if posHits >= 1:
             return int((json.loads(vtPos)['positives']))
         else:
-            return "No hits!"
+            return 0
     else:
-        return "IP Not found in DB."
+        return 0
 
 
 def toMongo(id):
-    db = mongoClient.yapdns
+    """
+    This function is called when both threads are finished. Then it will
+    create a variable records that contains all the information stored
+    in Redis with a certain id. Next it will insert the value to MongoDB.
+    Lastly it will give the value of record to the function hasher.
+    :param id: id has the value from teller in the function toRedis
+    """
     record = r_serv.hgetall("_id" + str(id))
     db.dnsinfo.insert_one(record)
     # build something to base the hash on
